9c49af8849894529227f799e156387360851fa59
Ekarat Rattagan <pokekarat@gmail.com>
2017-10-28 21:33:15 +0700

    Update TCPserver.py

--- TCPserver.py
+++ TCPserver.py
@@ -3,6 +3,7 @@
 
 import socket
 ServerSock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ServerSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 ServerSock.bind(('localhost',7007))
 ServerSock.listen(5)
 (NewClientSock, addr) = ServerSock.accept()
